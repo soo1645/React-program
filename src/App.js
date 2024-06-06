468304913d423908d21e756b9090f43c18e52eae
soo1645 <sooh1645@gmail.com>
2024-06-07 01:38:11 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,55 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Customer from './componemts/customer';
 import './App.css';
 
-function App() {
-  return (
-    <div className="gray-background">
-        <img src={logo} lat="logo" />
-        <h2>Let's develop management system!</h2>
+const customer = [
+  {
+    'id': 1,
+    'name': '이수현',
+    'birthday': '020111',
+    'gender': '여'
+  },
+  {
+    'id': 2,
+    'name': '홍길동',
+    'birthday': '011222',
+    'gender': '남'
+  }
+
+]
+
+class App extends Component{
+  render(){
+    return(
+      <div>
+        {/* <Customer
+          id={customer[0].id}
+          name={customer[0].name}
+          birthday={customer[0].birthday}
+          gender={customer[0].gender}
+        />
+        <Customer
+            id={customer[1].id}
+            name={customer[1].name}
+            birthday={customer[1].birthday}
+            gender={customer[1].gender}
+        /> */}
+        {
+          customer.map(c => {
+            return(
+              <Customer
+                key={c.id}
+                id={c.id}
+                name={c.name}
+                birthday={c.birthday}
+                gender={c.gender}
+              />
+            )
+          })
+        }
       </div>
-  );
+    );
+  }
 }
 
 export default App;
